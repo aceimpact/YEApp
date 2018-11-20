a976dae0a2a4e7802bdbc3c7a9394fe9a7857848
aceimpact <kei2613@icloud.com>
2018-11-20 17:25:23 +0900

    FirstCommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,27 @@ class UsersController < ApplicationController
 skip_before_action :authenticate_user!
 
   def index
-    
+
   end
 
   def show
-
+     @user = User.find_by!(id: params[:id])
   end
 
   def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
