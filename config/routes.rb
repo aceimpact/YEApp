e0a11625cc5edf543c61c5766d6246d337f6017d
aceimpact <kei2613@icloud.com>
2018-11-21 16:29:09 +0900

    beforeAddMypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  devise_for :users
 
   resources :users, :only => [:index, :show, :update, :destroy]
 
