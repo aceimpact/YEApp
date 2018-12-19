ff32d1646b4ddabae52124ba7e0c46b114b6df8f
aceimpact <kei2613@icloud.com>
2018-12-19 20:32:51 +0900

    ブランチ変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,15 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.create(user_id: current_user.id, video_id: params[:video_id])
+    @like = Like.create!(user_id: current_user.id, video_id: params[:video_id])
+    respond_to :js
     @likes = Like.where(video_id: params[:video_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, video_id: params[:video_id])
     like.destroy!
+    respond_to :js
     @likes = Like.where(video_id: params[:video_id])
   end
 end
