a2bf6d0b4602f815428da98368fff160b310bbcd
aceimpact <kei2613@icloud.com>
2018-12-21 16:38:46 +0900

    FirstCommit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,13 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.create!(user_id: current_user.id, video_id: params[:video_id])
-    respond_to :js
+    @like = current_user.likes.create!(video_id: params[:video_id])
     @likes = Like.where(video_id: params[:video_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, video_id: params[:video_id])
     like.destroy!
-    respond_to :js
     @likes = Like.where(video_id: params[:video_id])
   end
 end
