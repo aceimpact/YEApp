9d8201a4a31f8cc7d9d2e128a594d4d926e5d1f4
aceimpact <kei2613@icloud.com>
2018-12-29 13:58:04 +0900

    修正

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,4 @@
 class VideosController < ApplicationController
-
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_editable_video, only: [:edit, :update, :destroy]
 
