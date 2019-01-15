483953341e2852eeb313d6152bf8167b73837569
aceimpact <kei2613@icloud.com>
2019-01-15 13:20:38 +0900

    途中

---
+++ app/assets/javascripts/videos.js
@@ -0,0 +1,15 @@
+$(".twitter").click(function(){
+  window.open(url, ‘’, ‘width=580,height=400,menubar=no,toolbar=no,scrollbars=yes’);
+});
+var openShareWindow = function(url) {
+ window.open(url, ‘’, ‘width=580,height=400,menubar=no,toolbar=no,scrollbars=yes’);
+};
+
+var targetUrl = location.href
+
+var facebookShareUrl = `https://www.facebook.com/share.php?u=${targetUrl}`;
+var twitterShareUrl = `https://twitter.com/share?url=${targetUrl}`;
+
+var openShareWindow = function(url) {
+ window.open(url, ‘’, ‘width=580,height=400,menubar=no,toolbar=no,scrollbars=yes’);
+};
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      title:       "title",
+      title:       "YEApp",
       description: "description",
       keywords:    "rails,seo,meta-tags",
       #icon: [
@@ -9,7 +9,7 @@ module ApplicationHelper
         #{ href: image_url('sample.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       #],
       noindex: ! Rails.env.production?, # production環境以外はnoindex
-	    canonical: request.original_url,  # 優先されるurl
+	    canonical: request.url,  # 優先されるurl
       charset: "UTF-8",
 
       og: {
@@ -17,14 +17,13 @@ module ApplicationHelper
         description: :description,
         type: "website",
         url: request.original_url,
-        #image: image_url("OGPで設定する画像"),
+        image: image_url("スクリーンショット.png"),
         site_name: "site name",
         locale: "ja_JP"
       },
       twitter: {
-        site: '@ツイッターのアカウント名',
-        card: 'summary',
-        #image: image_url("sample_twitter.png")
+        card: 'summary'
+        image: image_url("スクリーンショット.png")
 	  },
       fb: {
         app_id: '465581603975903'    #ご自身のfacebookのapplication IDを設定
